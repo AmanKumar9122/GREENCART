e1a3b6a9f137e926e33b0d9ffa8c7842a8edd652
AmanKumar9122 <amanraj15112003@gmail.com>
2025-04-19 10:07:06 +0530

    categories selection updated

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,6 +14,7 @@ export const AppContextProvider = ({ children }) => {
     const [showUserLogin, setShowUserLogin] = useState(false);
     const [products, setProducts] = useState([]);
     const [cartItems, setCartItems] = useState({});
+    const [searchQuery, setSearchQuery] = useState({});
 
     // Fetch All Products
     const fetchProducts = async () => {
@@ -75,6 +76,8 @@ export const AppContextProvider = ({ children }) => {
         updateCartItem,
         removeFromCart,
         cartItems,
+        searchQuery,
+        setSearchQuery,
     };
 
     return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
