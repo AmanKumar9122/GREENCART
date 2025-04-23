c366ca2d8025beea2cc74f5b6cfcfb262246eef0
AmanKumar9122 <amanraj15112003@gmail.com>
2025-04-23 16:27:01 +0530

    created routes for authentication and logout

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,13 @@
 // import express from "express";
 import {Router} from "express";
-import { register } from "../controllers/userController.js";
+import { isAuth, login, logout, register } from "../controllers/userController.js";
+import authUser from "../middlewares/authUser.js";
 
 const userRouter = Router();
 
 userRouter.post("/register", register)
+userRouter.post("/login", login)
+userRouter.get("/is-auth",authUser, isAuth)
+userRouter.get("/logout",authUser, logout)
 
 export default userRouter;
